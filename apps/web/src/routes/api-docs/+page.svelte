cd123930cd5d78127115cf86a9d74344a82d79ec
Valentin Blanc <vblanc@z3r12p6.42lyon.fr>
2026-01-29 17:31:24 +0100

    feat: add openapi logic to the repo

---
+++ apps/web/src/routes/api-docs/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const script = document.createElement("script");
+    script.src = "/swagger-ui/swagger-ui-bundle.js";
+    script.onload = () => {
+      // @ts-ignore
+      const ui = SwaggerUIBundle({
+        dom_id: "#swagger",
+        url: "/openapi.yaml",
+        // @ts-ignore
+        presets: [SwaggerUIBundle.presets.apis],
+      });
+    };
+    document.body.appendChild(script);
+  });
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="/swagger-ui/swagger-ui.css" />
+</svelte:head>
+
+<div id="swagger"></div>
