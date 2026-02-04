f614fa66d25baef547cd46e78e28b8ca73dd5ec6
airone01 <21955960+airone01@users.noreply.github.com>
2026-02-04 20:50:52 +0100

    chore: patch vite config to remove unplugin icons

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,6 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -11,9 +10,6 @@ export default defineConfig({
     paraglideVitePlugin({
       project: "./project.inlang",
       outdir: "./src/lib/paraglide",
-    }),
-    Icons({
-      compiler: "svelte",
     }),
   ],
 });
