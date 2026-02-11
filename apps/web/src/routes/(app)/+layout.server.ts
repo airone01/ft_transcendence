362f2bcd342ca93284c2e97d763a62ae0be7f7c5
airone01 <21955960+airone01@users.noreply.github.com>
2026-02-11 18:15:19 +0100

    style: lint

--- apps/web/src/routes/(app)/+layout.server.ts
+++ apps/web/src/routes/(app)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals, url }) => {
+export const load: LayoutServerLoad = async ({ locals }) => {
   // This layout only runs for pages inside (app)
   if (!locals.user) {
     throw redirect(302, "/");
