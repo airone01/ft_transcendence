576e4e5a62f80ed0490ee306cefe84f8ac2eb9f4
airone01 <21955960+airone01@users.noreply.github.com>
2026-02-13 14:20:17 +0100

    fix(web): remove profile form component

--- apps/web/src/routes/(app)/settings/privacy/+page.svelte
+++ apps/web/src/routes/(app)/settings/privacy/+page.svelte
@@ -2,6 +2,6 @@
 import SettingsHeader from "$lib/components/settings-header.svelte";
 </script>
 
-<SettingsHeader title="Privacy." description="Your data and how users see you.">
+<SettingsHeader title="Privacy" description="Your data and how users see you.">
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, tempore illum sed rerum praesentium facere asperiores itaque pariatur nesciunt provident officia necessitatibus quas? Odit animi reiciendis natus perspiciatis sit. Minus?
 </SettingsHeader>
