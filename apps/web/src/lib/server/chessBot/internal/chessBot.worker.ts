e66a8797ca997e2febfdb06b73de8d622ec7779d
Valentin Blanc <vblanc@z3r12p6.42lyon.fr>
2026-02-25 14:38:31 +0100

    fix(chess-bot): update bot worker

--- apps/web/src/lib/server/chessBot/internal/chessBot.worker.ts
+++ apps/web/src/lib/server/chessBot/internal/chessBot.worker.ts
@@ -1,7 +1,27 @@
 import { parentPort, workerData } from "worker_threads";
-import { findBestMove } from "./bot/main";
+import { findBestMoveTimed } from "./bot/main";
 
 const { state } = workerData;
-const bestMove = findBestMove(state);
+const bestMove = findBestMoveTimed(state);
 
 parentPort?.postMessage({ bestMove });
+
+// Into .../+page.server.ts (to check if worker is working as expected)
+
+/* import { getBotMove } from "$lib/server/chessBot";
+import type { GameState } from "$lib/chess";
+import { fail } from "@sveltejs/kit";
+
+export const actions = {
+  move: async ({ request }: { request: Request }) => {
+    const data = await request.formData();
+    const state = data.get("state") as GameState | null;
+
+    if (!state) return fail(400);
+
+    const bestMove = await getBotMove(state);
+
+    return { bestMove };
+  },
+};
+ */
