53f81079b6621048cf245ee1bb43fe3dd07862a1
Valentin Blanc <vblanc@z3r4p5.42lyon.fr>
2026-02-27 14:53:41 +0100

    chore(chess-bot): fill index.ts

--- apps/web/src/lib/server/chessBot/index.ts
+++ apps/web/src/lib/server/chessBot/index.ts
@@ -1,1 +1,27 @@
+// Run `bun run ./apps/web/src/lib/server/chessBot/internal/test.bot.ts` to see the bot play
+
+// To use bot without WebWorker, use this instead as `findBestMoveTimed(state)`
+export { findBestMoveTimed } from "./internal/bot/main";
+
+// Use this to use WebWorker
 export { getBotMove } from "./internal/chessBot";
+
+// Into .../+page.server.ts (to check if worker is working as expected)
+
+/* import { getBotMove } from "$lib/server/chessBot";
+import type { GameState } from "$lib/chess";
+import { fail } from "@sveltejs/kit";
+
+export const actions = {
+  move: async ({ request }: { request: Request }) => {
+    const data = await request.formData();
+    const state = data.get("state") as GameState | null;
+
+    if (!state) return fail(400);
+
+    const bestMove = await getBotMove(state);
+
+    return { bestMove };
+  },
+};
+ */
--- apps/web/src/lib/server/chessBot/internal/chessBot.worker.ts
+++ apps/web/src/lib/server/chessBot/internal/chessBot.worker.ts
@@ -5,23 +5,3 @@ const { state } = workerData;
 const bestMove = findBestMoveTimed(state);
 
 parentPort?.postMessage({ bestMove });
-
-// Into .../+page.server.ts (to check if worker is working as expected)
-
-/* import { getBotMove } from "$lib/server/chessBot";
-import type { GameState } from "$lib/chess";
-import { fail } from "@sveltejs/kit";
-
-export const actions = {
-  move: async ({ request }: { request: Request }) => {
-    const data = await request.formData();
-    const state = data.get("state") as GameState | null;
-
-    if (!state) return fail(400);
-
-    const bestMove = await getBotMove(state);
-
-    return { bestMove };
-  },
-};
- */
