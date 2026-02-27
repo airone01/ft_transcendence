dc5f04edc002ceea4081098d73983dcfc800dfc2
Valentin Blanc <vblanc@z3r4p5.42lyon.fr>
2026-02-27 13:36:04 +0100

    feat: little win-ratio-card rework

--- apps/web/src/routes/(app)/profile/[id]/win-ratio-card.svelte
+++ apps/web/src/routes/(app)/profile/[id]/win-ratio-card.svelte
@@ -30,16 +30,28 @@ const { stats }: { stats: UserStats } = $props();
   <CardContent class="flex-1 flex flex-col justify-center">
     {#if stats.gamesPlayed !== 0}
       <div
-        class="flex h-5 w-full rounded-full overflow-hidden mb-3 shadow-inner"
+        class="flex h-10 w-full rounded-full overflow-hidden mb-3 shadow-inner"
       >
-        <div style="flex: {stats.wins}" class="bg-emerald-500 h-full"></div>
-        <div style="flex: {stats.draws}" class="bg-slate-400 h-full"></div>
-        <div style="flex: {stats.losses}" class="bg-rose-500 h-full"></div>
+        <div style="flex: {stats.wins}" class="text-primary bg-emerald-500 h-full"></div>
+        <div style="flex: {stats.draws}" class="text-primary bg-slate-400 h-full"></div>
+        <div style="flex: {stats.losses}" class="text-primary bg-rose-500 h-full"></div>
       </div>
-      <div class="flex justify-between text-sm font-medium">
-        <span class="text-emerald-600">{stats.wins} W</span>
-        <span class="text-slate-500">{stats.draws} D</span>
-        <span class="text-rose-600">{stats.losses} L</span>
+      <div class="flex w-full text-sm font-medium">
+        {#if stats.wins > 0}
+          <span style="flex: {stats.wins}" class="text-center text-emerald-600"
+            >{stats.wins} W</span
+          >
+        {/if}
+        {#if stats.draws > 0}
+          <span style="flex: {stats.draws}" class="text-center text-slate-500"
+            >{stats.draws} D</span
+          >
+        {/if}
+        {#if stats.losses > 0}
+          <span style="flex: {stats.losses}" class="text-center text-rose-600"
+            >{stats.losses} L</span
+          >
+        {/if}
       </div>
     {/if}
   </CardContent>
