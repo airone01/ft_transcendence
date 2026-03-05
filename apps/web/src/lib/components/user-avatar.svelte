94a3fff7eb46a6072c0643bb1b0302ed07841696
Valentin Blanc <vblanc@z1r4p3.42lyon.fr>
2026-03-05 19:43:40 +0100

    feat(i18n): add i18n to resolve game mode

--- apps/web/src/lib/components/user-avatar.svelte
+++ apps/web/src/lib/components/user-avatar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { Avatar, AvatarFallback, AvatarImage } from "@transc/ui/avatar";
 import { onlineUsersStore } from "$lib/stores/presence.store";
+  import { m } from "$lib/paraglide/messages";
 
 let {
   userId,
@@ -24,6 +25,17 @@ let statusColorClass = $derived(
       ? "bg-purple-500"
       : "bg-muted-foreground",
 );
+
+let resolveStatus = (status: string) => {
+  switch (status) {
+    case "online":
+      return m.online();
+    case "offline":
+      return m.offline();
+    case "ingame":
+      return m.ingame();
+  }
+}
 </script>
 
 <div class="relative inline-flex shrink-0 align-middle">
@@ -33,6 +45,6 @@ let statusColorClass = $derived(
   </Avatar>
   <span
     class="absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-background {statusColorClass}"
-    title={status}
+    title={resolveStatus(status)}
   ></span>
 </div>
