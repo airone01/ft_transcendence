feb1fe54f0e439bf303667dd7582fa52a4405da4
Valentin Blanc <vblanc@z1r4p3.42lyon.fr>
2026-03-05 15:34:56 +0100

    feat(i18n): add i18n to profile/me page

--- apps/web/src/routes/(app)/profile/me/social/+page.server.ts
+++ apps/web/src/routes/(app)/profile/me/social/+page.server.ts
@@ -11,6 +11,7 @@ import {
   dbRequestFriendship,
 } from "$lib/server/db-services";
 import type { Actions, PageServerLoad } from "./$types";
+import * as m from "$lib/paraglide/messages";
 
 export const load: PageServerLoad = async ({ locals }) => {
   if (!locals.user)
@@ -38,25 +39,32 @@ export const actions: Actions = {
     const formData = await request.formData();
     const username = formData.get("username")?.toString();
 
-    if (!username) return fail(400, { error: "Username is required" });
+    if (!username)
+      return fail(400, {
+        error: m.profile_page_action_add_username_required(),
+      });
 
     if (username === locals.user.username)
-      return fail(400, { error: "You cannot add yourself" });
+      return fail(400, { error: m.profile_page_action_add_add_yourself() });
 
     try {
       const targetUser = await dbGetUserByUsername(username);
-      if (!targetUser) return fail(404, { error: "User not found" });
+      if (!targetUser)
+        return fail(404, { error: m.profile_page_action_add_user_not_found() });
 
       await dbRequestFriendship(locals.user.id, targetUser.id);
-      return { success: true, message: `Sent friend request to ${username}` };
+      return {
+        success: true,
+        message: m.profile_page_action_add_success({ username }),
+      };
     } catch (error) {
       if (error instanceof DBAddFriendFriendshipAlreadyExistsError) {
         return fail(400, {
-          error: "You are already friends or an invite is pending",
+          error: m.profile_page_action_add_fail(),
         });
       }
       console.error(error);
-      return fail(500, { error: "Failed to send friend request" });
+      return fail(500, { error: m.profile_page_action_add_internal_error() });
     }
   },
 
@@ -67,16 +75,23 @@ export const actions: Actions = {
     const senderId = Number(formData.get("userId"));
 
     if (!senderId || Number.isNaN(senderId))
-      return fail(400, { error: "Invalid user ID" });
+      return fail(400, {
+        error: m.profile_page_action_accept_invalid_userid(),
+      });
 
     try {
       await dbAddFriend(locals.user.id, senderId);
       await dbRejectFriendship(senderId, locals.user.id);
 
-      return { success: true, message: "Friend request accepted" };
+      return {
+        success: true,
+        message: m.profile_page_action_accept_success(),
+      };
     } catch (error) {
       console.error(error);
-      return fail(500, { error: "Failed to accept friend request" });
+      return fail(500, {
+        error: m.profile_page_action_accept_internal_error(),
+      });
     }
   },
 
@@ -87,14 +102,21 @@ export const actions: Actions = {
     const senderId = Number(formData.get("userId"));
 
     if (!senderId || Number.isNaN(senderId))
-      return fail(400, { error: "Invalid user ID" });
+      return fail(400, {
+        error: m.profile_page_action_reject_invalid_userid(),
+      });
 
     try {
       await dbRejectFriendship(senderId, locals.user.id);
-      return { success: true, message: "Friend request rejected" };
+      return {
+        success: true,
+        message: m.profile_page_action_reject_success(),
+      };
     } catch (error) {
       console.error(error);
-      return fail(500, { error: "Failed to reject friend request" });
+      return fail(500, {
+        error: m.profile_page_action_reject_internal_error(),
+      });
     }
   },
 
@@ -105,15 +127,19 @@ export const actions: Actions = {
     const friendId = Number(formData.get("friendId"));
 
     if (!friendId || Number.isNaN(friendId)) {
-      return fail(400, { error: "Invalid friend ID" });
+      return fail(400, {
+        error: m.profile_page_action_remove_invalid_userid(),
+      });
     }
 
     try {
       await dbRemoveFriend(locals.user.id, friendId);
-      return { success: true, message: "Friend removed" };
+      return { success: true, message: m.profile_page_action_remove_success() };
     } catch (error) {
       console.error(error);
-      return fail(500, { error: "Failed to remove friend" });
+      return fail(500, {
+        error: m.profile_page_action_remove_internal_error(),
+      });
     }
   },
 };
